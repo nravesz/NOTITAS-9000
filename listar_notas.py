9aad963d01f9c9814b5ffa0b282333c0f66f3f0c
xNicky <nraveszani@gmail.com>
2016-10-06 15:07:39 -0300

    Add files via upload

---
+++ listar_notas.py
@@ -0,0 +1,6 @@
+def listar_notas(diccionario):
+    """Recibe un diccionario como parámetro. Imprime su contenido en forma de lista"""
+    for etiqueta,texto in diccionario.items():
+        print("[{}]".format(etiqueta))
+        for casillero in texto:
+            print("-{}".format(casillero))
