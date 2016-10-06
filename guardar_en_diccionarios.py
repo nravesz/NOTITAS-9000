04a383ced2f229becbbec76c9974c8a0a5fcbdfc
xNicky <nraveszani@gmail.com>
2016-10-06 17:08:48 -0300

    Update guardar_en_diccionarios.py

--- guardar_en_diccionarios.py
+++ guardar_en_diccionarios.py
@@ -1,5 +1,5 @@
 def guardar_en_diccionarios():
-    """Convierte los datos de notas.csv en un diccionario. La clave es la etiqueta de la nota y el valor una lista con eltexto"""
+    """Convierte los datos de notas.csv en un diccionario. La clave es la etiqueta de la nota y el valor una lista con el texto"""
     diccionario = {}
     with open("notas.csv","r") as f:
         for linea in f:
