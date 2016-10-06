aa22dc6a555d788fcd889d40d30f1e1cba68620c
xNicky <nraveszani@gmail.com>
2016-10-06 16:15:19 -0300

    Update buscar_nota.py

--- buscar_nota.py
+++ buscar_nota.py
@@ -1,13 +1,14 @@
 def buscar_nota(diccionario):
-    """Le pide al usuario un texto clave. Busca en los valores del diccionario dicho texto Le muestra al usuario en qué notas se encuentra la palabra"""
+    """Le pide al usuario un texto clave. Busca en los valores del diccionario dicho texto. Muestra al usuario en qué notas se encuentra la palabra"""
+    resultados = {}
     clave = input("Ingrese el texto que desea buscar: ")
     clave_minus = clave.lower()
     for etiqueta,texto in diccionario.items():
         for casillero in texto:
             casillero_minus = casillero.lower()
             if clave_minus in casillero_minus:
-                print("[{}]".format(etiqueta))
-                print("-{}".format(casillero))
-                print("\n")
-    #Decirle al usuario que no existe lo que buscó
-    #Hacer algo si el usuario ingresa un campo vacío
+                resultados[etiqueta] = resultados.get(etiqueta,[]) + [casillero]
+    if resultados == {}:
+        print("No se han encontrado coincidencias")
+    else:
+        coincidencias = listar_notas.listar_notas(resultados)
